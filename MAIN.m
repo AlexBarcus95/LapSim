ace3de68dea103a44bb1d23760d1e421c82d10d4
Alex Barcus <alexanderb2013@gmail.com>
2020-11-15 22:03:01 +0000

    Solving algorithm changed from interior point to sqp for improved performance with the non-linear tyre model.

--- MAIN.m
+++ MAIN.m
@@ -60,9 +60,9 @@ v_old = customGuess.soln.grid.state(1,:);
 T_old = customGuess.soln.grid.control(1,:);
 a_old = customGuess.soln.grid.control(2,:);
 
-v_guess = interp1(sLap_old, v_old, problem.dsSystem.td.sLap).*0.95;
+v_guess = interp1(sLap_old, v_old, problem.dsSystem.td.sLap);
 T_guess = interp1(sLap_old, T_old, problem.dsSystem.td.sLap);
-a_guess = interp1(sLap_old, a_old, problem.dsSystem.td.sLap).*0.8;
+a_guess = interp1(sLap_old, a_old, problem.dsSystem.td.sLap);
 
 problem.guess.state     = v_guess;
 problem.guess.control   = [T_guess;
@@ -73,8 +73,9 @@ problem.guess.control   = [T_guess;
 problem.options.nlpOpt = optimset();
 problem.options.nlpOpt.Display = 'iter';
 problem.options.nlpOpt.MaxFunEvals = 1e6;
-problem.options.nlpOpt.MaxIter = 2e3;
+problem.options.nlpOpt.MaxIter = 3e3;
 problem.options.nlpOpt.TolFun = 1e-7;
+problem.options.nlpOpt.Algorithm = 'sqp';
 
 soln = Solver.LTS(problem);
 
